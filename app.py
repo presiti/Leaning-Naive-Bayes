47bfe0c2652714b793aa49fbcb319c6c946407e8
zinna0347 <zinna0347@datacentric.kr>
2023-12-21 17:59:46 +0900

    class별로 feature들 정규분포 찍기

--- app.py
+++ app.py
@@ -5,8 +5,50 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from bokeh.plotting import figure
 # from scipy.stats import norm
+
+def normal_chart(df, c, f, chart):
+    iris = df[df['variety']==c]
+    # iris_Versicolor = iris_pd[iris_pd['variety']=='Versicolor']
+    # iris_Virginica = iris_pd[iris_pd['variety']=='Virginica']
+
+    f_np = []       # feature별 표준편차 값 list
+    mu = []
+    sigma = []
+
+    pi = np.pi      # pie
+    length = 50
+
+    # 정규분포 구하기
+    for h in range(len(f)): # 4번 반복
+        f_np.append(iris[f[h]].sort_values().to_numpy())
+        mu.append(np.mean(f_np[h]))    # 평균
+        sigma.append(np.std(f_np[h]))  # 표준편차
+        
+        for i in range(length): # 정규분포
+            f_np[h][i] = 1/sigma[h]*np.sqrt(2*pi)*np.exp(-(f_np[h][i]-mu[h])**2/(2*(sigma[h]**2)))
+
+    # 출력하기
+    print('select chat : ', chart)
+    if(chart=='scatter'):
+        chart = pd.DataFrame()
+        for i in range(4):
+            chart[f[i]] = f_np[i]
+        st.scatter_chart(chart)
+    elif(chart=='line'):
+        fig, ax = plt.subplots()
+        for i in range(len(f_np)):
+            ax.plot(f_np[i])
+        st.pyplot(fig)
+    elif(chart=='hist'):
+        pass
+    else:
+        pass
+    
+    # 둘중 선택할 수 있게 하기 - selectbox
+    # 선택후 출력된 그래프 이미지로 다운로드 할 수 있게 하기
+
 def main():
-    print('======================= start')
+    print('=============================start stremlit')
     iris_pd = pd.read_csv("iris.csv")
 
     st.title("IRIS Dataset! :bouquet:")
@@ -18,6 +60,9 @@ def main():
     print(iris_pd.isnull().sum())
     print()
 
+
+    # Dataset Info
+
     #show dataset head
     st.subheader('iris :violet[sample]', divider='violet')
     st.dataframe(data=iris_pd.head())
@@ -26,7 +71,9 @@ def main():
     st.subheader('iris :violet[class info]', divider='violet')
     iris_class = iris_pd.groupby('variety')['variety'].count()
     st.dataframe(data=iris_class)
-    
+    print('===================출력')
+    print(iris_class)
+    print()
 
     #show dataset describe
     st.subheader('iris :violet[describe]', divider='violet')
@@ -34,60 +81,37 @@ def main():
     st.dataframe(iris_pd.describe())
 
 
-    iris_Setosa = iris_pd[iris_pd['variety']=='Setosa']
-    # iris_Versicolor = iris_pd[iris_pd['variety']=='Versicolor']
-    # iris_Virginica = iris_pd[iris_pd['variety']=='Virginica']
-
+    # Graph
 
     #show scatter chart
     st.subheader('iris :violet[scatter chart]', divider='violet')
     # st.scatter_chart(iris_pd)
-    
 
-    #show normal distribution
+    # show normal distribution
     st.subheader('iris :violet[draw normal distribution]', divider='violet')
 
-    x1 = iris_Setosa['sepal.length'].sort_values().to_numpy()
-    print('class : Setosa, feature : sepal.length')
-    print(x1)
-    print()
-    # st.markdown("x1 length")
-    # st.caption(len(x1))
-    # print('==================== x1 type: ',type(x1))
-    
-    mu = np.mean(x1)        # 평균
-    print('평균 : ',mu)
-    sigma = (np.std(x1))    # 표준편차
-    print('표준편차 : ',sigma)
-    pi = np.pi              # 파이
-    print()
-
-    # 정규분포 수식
+    # formula
     st.latex(r'''
             N(x∣μ,σ^2)≡\frac{1}{σ\sqrt{2π}}\exp[−\frac{(x−μ)^2}{2σ^2}]
             ''')
-
-    # 정규분포(y) 구하기
-    y1 = x1
-    for i in range(len(x1)):
-        y1[i] = 1/sigma*np.sqrt(2*pi)*np.exp(-(y1[i]-mu)**2/(2*(sigma**2)))
-    print('정규분포')
-    print(y1)
-    print()
-
-    #출력하기
-    st.markdown("##### matplot")
-    st.markdown("Setosa-sepal.length Origin")
-    fig, ax = plt.subplots()
-    ax.plot(x1)
-    st.pyplot(fig)
-
-    st.markdown("Setosa-sepal.normal distribution")
-    fig, ax = plt.subplots()
-    ax.plot(y1)
-    st.pyplot(fig)
-
-    st.scatter_chart(y1)
     
+    # select class
+    select_class = st.selectbox(
+        'selct a class',
+        ('Setosa', 'Versicolor', 'Virginica')
+    )
+
+    # select chart type
+    select_normal_chart = st.radio(
+        'select a chart type',
+        ('scatter','line','hist')
+    )
+    st.markdown('#### Setosa')
+    # c_list = ['Setosa', 'Versicolor', 'Virginica']
+    f_list = ['sepal.length', 'sepal.width','petal.length','petal.width']
+    normal_chart(iris_pd, select_class, f_list, select_normal_chart)
+
+    # st.dataframe(iris_pd[iris_pd['variety']==(i for i in c_list)])
+
 if __name__=='__main__':
     main()
