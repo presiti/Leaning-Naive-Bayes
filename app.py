13f09bfa5b8677926e2251b0a677c20d81609193
persiti <zinna0347@gmail.com>
2023-12-27 18:01:33 +0900

    데이터 구조 변환 진행 중

--- app.py
+++ app.py
@@ -14,13 +14,13 @@ def main():
 
     # null check
     print('결측치 확인')
-    print(iris_pd.isnull().sum())
-    print()
+    print(iris_pd.isnull().sum(),'\n')
 
 
     # Dataset Info
+    print('show dataset info\n')
 
-    #show dataset head
+    #show dataset head(sample)
     st.subheader('iris :violet[sample]', divider='violet')
     st.dataframe(data=iris_pd.head())
 
@@ -28,7 +28,6 @@ def main():
     st.subheader('iris :violet[class info]', divider='violet')
     iris_class = iris_pd.groupby('variety')['variety'].count()
     st.dataframe(data=iris_class)
-    st.dataframe(data=iris_pd.groupby('variety'))
 
     #show dataset describe
     st.subheader('iris :violet[describe]', divider='violet')
@@ -37,29 +36,64 @@ def main():
 
 
     # Graph
+    c_list = ['Setosa', 'Versicolor', 'Virginica']                          #class name list
+    f_list = [iris_pd.columns[i] for i in range(4)]                         #feature name list
 
-    #show scatter chart
+    #show Scatter chart
     st.subheader('iris :violet[scatter chart]', divider='violet')
-    # st.scatter_chart(iris_pd)
 
-    # show normal distribution
+    st.dataframe(iris_pd)
+    iris_trans=pd.DataFrame()
+
+    # 테스트1 : 깡
+    iris_temp = iris_pd[iris_pd.variety==c_list[0]]             # 1. 클래스별로 우선 분리
+    # st.dataframe(iris_temp)
+
+    # 2. 분리 후 특정 행 추출 확인
+    st.dataframe(iris_temp.loc[:, [f_list[0], 'variety']])          
+    # print(type(iris_temp.loc[:, f_list[0]]))                # pandas Series                              
+    
+    # 3. 분리 후 각 feature 한 줄 넣기
+    iris_trans[c_list[0]] = iris_temp.loc[:, f_list[0]]
+    st.dataframe(iris_trans)
+
+    iris_trans=pd.DataFrame()
+
+    # 4. class별로 다 넣기
+    # 그냥 넣기
+    for i in range(3):
+        iris_temp = iris_pd[iris_pd.variety==c_list[i]]
+        iris_trans[c_list[i]] = [(iris_temp.loc[:, f_list[j]]) for j in range(4)]
+    st.dataframe(iris_trans)
+
+    iris_trans['feature']=f_list[0]                              # 4. feature 행 추가하기
+    st.dataframe(iris_trans.explode(c_list))
+
+    # 테스트1-2:concat, axis 써보기
+    # st.scatter_chart(iris_trans)
+
+    
+    # 테스트2 : transpose()         
+    iris_temp=st.dataframe(iris_pd.transpose())                 # 1. .transpos()로 행열 바꾸기
+
+    st.dataframe(iris_temp.grouby('variety'))
+
+    
+    # show Normal Distribution
     st.subheader('iris :violet[draw normal distribution]', divider='violet')
 
     # formula
     st.latex(r'''
             N(x∣μ,σ^2)≡\frac{1}{σ\sqrt{2π}}\exp[−\frac{(x−μ)^2}{2σ^2}]
             ''')
-    c_list = ['Setosa', 'Versicolor', 'Virginica']                          #class name list
-    f_list = [iris_pd.columns[i] for i in range(4)]                         #feature name list
-    print('===================출력')
-    print(f_list)
-    print()
 
     # select chart type
     select_chart_type = st.radio(
         'select a chart type',
         ('scatter', 'line', 'hist')
     )
+    norm = nc.normal_chart(iris_pd)
+
 
     st.markdown('### by Class')
     # select class
@@ -67,9 +101,7 @@ def main():
         'selct a class',
         ('Setosa', 'Versicolor', 'Virginica')
     )
-    nc.by_class(iris_pd, select_class, f_list, select_chart_type)
-    
-    
+    norm.by_class(select_class, f_list, select_chart_type)
 
     st.markdown('### by Feature')
     #select Feature
@@ -77,7 +109,7 @@ def main():
         'select a feature',
         (f_list)
     )
-    nc.by_feature(iris_pd, c_list, select_feature, select_chart_type)
+    # norm.by_feature(c_list, select_feature, select_chart_type)
 
     
 
--- normalChart.py
+++ normalChart.py
@@ -5,77 +5,78 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from bokeh.plotting import figure
 
-def by_class(df, c, f, chart):
-    print('---------------------class별로 feature 정규분포 그리기')
-    iris = df[df['variety']==c]
-    np_list = []         # 표준편차 값 list
-    mu = []
-    sigma = []
-    pi = np.pi      # pie
+class normal_chart:
+    def __init__(self, df):
+        self.df = df
+        self.pi = np.pi      # pie
 
-    # 정규분포 구하기
-    for h in range(len(f)): # 4번 반복
-        np_list.append(iris[f[h]].sort_values().to_numpy())
-        mu.append(np.mean(np_list[h]))    # 평균
-        sigma.append(np.std(np_list[h]))  # 표준편차
+    def by_class(self, c, f, chart):
+        print('---------------------class별로 feature 정규분포 그리기')
+        iris = self.df[self.df.variety==c]
+        np_list = []         # 표준편차 값 list
+        mu = []
+        sigma = []
+        
+        # 정규분포 구하기
+        for h in range(len(f)): # 4번 반복
+            np_list.append(iris[f[h]].sort_values().to_numpy())
+            mu.append(np.mean(np_list[h]))    # 평균
+            sigma.append(np.std(np_list[h]))  # 표준편차
 
-        for i in range(50): # 정규분포
-            np_list[h][i] = 1/sigma[h]*np.sqrt(2*pi)*np.exp(-(np_list[h][i]-mu[h])**2/(2*(sigma[h]**2)))
+            for i in range(50): # 정규분포
+                np_list[h][i] = 1/sigma[h]*np.sqrt(2*self.pi)*np.exp(-(np_list[h][i]-mu[h])**2/(2*(sigma[h]**2)))
 
-    print('정규분포 계산 완료 :',np_list[0][0])
+        print('정규분포 계산 완료 :',np_list[0][0])
 
-    # 출력하기
-    st.markdown('#### 'f'{c}')
+        # 출력하기
+        st.markdown('#### 'f'{c}')
 
-    legend = []
-    fig, ax = plt.subplots()
-    ax.set_title('normal distribution')
+        legend = []
+        fig, ax = plt.subplots()
+        ax.set_title('normal distribution')
 
-    print('그래프 종류 : ', chart)
-    print()
-    if(chart=='scatter'):
-        chart = pd.DataFrame()
-        for i in range(4):
-            chart[f[i]] = np_list[i]
-        st.scatter_chart(chart)
+        print('그래프 종류 : ', chart)
+        print()
+        if(chart=='scatter'):
+            chart = pd.DataFrame()
+            for i in range(4):
+                chart[f[i]] = np_list[i]
+            st.scatter_chart(chart)
 
-    elif(chart=='line'):
-        for i in range(len(f)):
-            ax.plot(np_list[i], alpha=0.7)
-            legend.append(f'{iris.columns[i]}')
+        elif(chart=='line'):
+            for i in range(len(f)):
+                ax.plot(np_list[i], alpha=0.7)
+                legend.append(f'{iris.columns[i]}')
 
-        ax.legend(legend)
-        st.pyplot(fig)
+            ax.legend(legend)
+            st.pyplot(fig)
 
-    elif(chart=='hist'):
-        for i in range(len(f)):
-            ax.hist(np_list[i], alpha=0.7)
-            legend.append(f'{iris.columns[i]}')
-        ax.legend(legend)
-        st.pyplot(fig)
-    else:
-        pass
-    # 선택후 출력된 그래프 이미지로 다운로드 할 수 있게 하기
+        elif(chart=='hist'):
+            for i in range(len(f)):
+                ax.hist(np_list[i], alpha=0.7)
+                legend.append(f'{iris.columns[i]}')
+            ax.legend(legend)
+            st.pyplot(fig)
+        else:
+            pass
+        # 선택후 출력된 그래프 이미지로 다운로드 할 수 있게 하기
 
-def by_feature(df, c, f, chart):
-    print('---------------------feature별로 feature 정규분포 그리기')
-    # 하고 싶은 것
-    # 선택한 feature(column)을 variety별로 나눠서 저장.=> column을 variety로, row를 feature로 변환하여 저장
+    def by_feature(self, c, f, chart):
+        print('---------------------feature별로 feature 정규분포 그리기')
+        # 하고 싶은 것
+        # 선택한 feature(column)을 variety별로 나눠서 저장.=> column을 variety로, row를 feature로 변환하여 저장
 
-    # 수행 순서
-    # column이 class인 데이터 프레임 생성 
-    iris = pd.DataFrame({
-        i:[] for i in c
-        })
-    print(iris.head())
-    print()
-    st.dataframe(iris)
+        # 수행 순서
+        # column이 class인 데이터 프레임 생성 
+        iris = pd.DataFrame({
+            i:[] for i in c
+            })
+        st.dataframe(iris)
 
-    # class 명에 맞는 feature값 채워넣기 => groupbt 수행
-    print(df.groupby('variety')) 
-    
-    np_list = []         # 표준편차 값 list
-    mu = []
-    sigma = []
-    pi = np.pi      # pie
-    
+        # class 명에 맞는 feature값 채워넣기 => groupbt 수행
+        print(self.df.groupby('variety')) 
+        
+        np_list = []         # 표준편차 값 list
+        mu = []
+        sigma = []
+        
